cfc56dca47c5f58788275349be8c06aef5efeb92
Ansarissab <zahidensari116@gmail.com>
2022-07-18 15:54:51 +0500

    Fixed alignments

--- js/index.js
+++ js/index.js
@@ -1,34 +1,23 @@
-document.querySelector("#mountain1-btn").addEventListener('click', function(){
-  console.log('btn1 clicked')
-  document.querySelector('.mountain1').classList.add('shown')
-  document.querySelector('.mountain1').classList.remove('hidden')
+document.querySelector("#mountain1-btn").addEventListener("click", function () {
+  toggleClasses(1, 2);
+});
 
-  document.querySelector('.mountain2').classList.remove('shown')
-  document.querySelector('.mountain2').classList.add('hidden')
+document.querySelector("#mountain2-btn").addEventListener("click", function () {
+  toggleClasses(2, 1);
+});
 
-  document.querySelector('#mountain1-btn')
-  .parentElement
-  .classList.add('active')
+function toggleClasses(self_id, opponent_id) {
+  document.querySelector(`.mountain${self_id}`).classList.add("shown");
+  document.querySelector(`.mountain${self_id}`).classList.remove("hidden");
 
-  document.querySelector('#mountain2-btn')
-  .parentElement
-  .classList.remove('active')
-})
+  document.querySelector(`.mountain${opponent_id}`).classList.remove("shown");
+  document.querySelector(`.mountain${opponent_id}`).classList.add("hidden");
 
-document.querySelector("#mountain2-btn").addEventListener('click', function(){
-  console.log('btn2 clicked')
+  document
+    .querySelector(`#mountain${self_id}-btn`)
+    .parentElement.classList.add("active");
 
-  document.querySelector('.mountain2').classList.add('shown')
-  document.querySelector('.mountain2').classList.remove('hidden')
-
-  document.querySelector('.mountain1').classList.remove('shown')
-  document.querySelector('.mountain1').classList.add('hidden')
-
-  document.querySelector('#mountain2-btn')
-  .parentElement
-  .classList.add('active')
-
-  document.querySelector('#mountain2-btn')
-  .parentElement
-  .classList.remove('active')
-})
+  document
+    .querySelector(`#mountain${opponent_id}-btn`)
+    .parentElement.classList.remove("active");
+}
